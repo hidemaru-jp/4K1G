97040a0be5476530d6d5ade353e1fd82aa6af8bb
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-11-06 07:57:30 +0000

    ADD devise user&admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  # ユーザー用(パスワード機能除外)
+  # URL /customers/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用（管理者登録・パスワード機能除外）
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+　
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
