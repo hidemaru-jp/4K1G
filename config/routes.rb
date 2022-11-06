e0eddd50b4010f78111e9fcb4cd4a9c802917b67
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-11-06 11:17:22 +0000

    devise login available

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-　
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "public/homes#top"
+  get '/about' => "public/homes#about", as: "about"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(
+Admin.create!(
   name: 'admin',
   email: 'test@test',
   password: 'password'
