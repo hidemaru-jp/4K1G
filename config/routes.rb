f9fbac4217cfb13ffc705b6260c2b8b900da3487
hide <111981295+hidemaru-jp@users.noreply.github.com>
2022-11-07 09:49:38 +0900

    Revert "Post"

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,3 @@
 Rails.application.routes.draw do
-
-  # ユーザー用(パスワード機能除外)
-  # URL /customers/sign_in ...
-  devise_for :users,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
-  # 管理者用（管理者登録・パスワード機能除外）
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root to: "public/homes#top"
-  get '/about' => "public/homes#about", as: "about"
-
-  scope module: :public do
-    resources :users
-    resources :posts
-  end
-
-
 end
