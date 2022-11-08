e411eed1dc54a6fd71bb1be3af9de961fb035a17
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-11-08 10:42:16 +0000

    ADD favorite function

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
     resources :users
     resources :posts do
       resources :post_comments
+      resource :favorites, only: [:create, :destroy]
     end
   end
 
