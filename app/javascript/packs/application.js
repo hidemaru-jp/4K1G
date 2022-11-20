8a1a06c69ee7e45c3e5eb05dd683db4afb20c228
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-11-20 11:29:41 +0000

    FIX stylesheet test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@ import "bootstrap";
 import "../stylesheets/application"
 import Chart from "chart.js/auto";
 global.Chart = Chart;
-import 'stylesheets/application'
+import "stylesheets/test"
 
 Rails.start()
 Turbolinks.start()
